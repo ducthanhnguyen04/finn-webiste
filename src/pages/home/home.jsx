981502a3ba6a8d394c6770aba774231dce80727e
Đức Thanh <ken@kens-MacBook-Pro.local>
2026-07-01 00:13:23 +0800

    update code

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
 import "./home.css";
 import HeroSlider from "../../components/heroSlider/heroSlider";
-import img1 from "../../assets/1.png";
-import img2 from "../../assets/2.png";
-import img3 from "../../assets/3.png";
-import img4 from "../../assets/4.png";
-import img5 from "../../assets/5.png";
-import img6 from "../../assets/6.png";
 import vct1 from "../../assets/vct/VCT 1.png";
 import vct2 from "../../assets/vct/VCT 2.png";
 import vct3 from "../../assets/vct/VCT 3.png";
